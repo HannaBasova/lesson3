5ad65c9ffa028a073f032f8baf1b10fa3897c487
Hanna <anna.potolok@gmail.com>
2025-07-15 18:08:40 +0200

    functions to check odd numbers

--- debugger.py
+++ debugger.py
@@ -5,12 +5,29 @@
 '''
 
 
+def is_even(number: int)->bool:
+    '''
+    Function that receives numbers and returns True if the number is even
+    :param number: int
+    :return: bool
+    '''
+    return  number % 2 == 0
 
-list = [1,2,3,4,5,6,7,8,9]
 
-def odd_numbers(lst: list) -> int:
-    for chars in lst: #пройтися по усім елементам списку
-        if chars % 2 != 0: # перевірка на непарність
-            print(chars, end = ' ') # якщо непарні друкуємо
+print(is_even(5))
 
-odd_numbers(list)
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+def print_odd_numbers(lst: list[int]):
+    '''
+    a function that receives a list of numbers and prints out the odd numbers
+    :param lst: list
+    :return: None
+    '''
+    for chars in lst:  #пройтися по усім елементам списку
+        if chars % 2 != 0:  # перевірка на непарність
+            print(chars, end=' ')  # якщо непарні друкуємо
+
+
+print_odd_numbers(list)
